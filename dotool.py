ea45a51c348d10bc9ff1b8e1a7b0553778194ec6
Malcolm VanOrder <mvanorder1390@gmail.com>
2016-10-30 00:26:26 -0400

    Start argparse config
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- dotool.py
+++ dotool.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import sys
+from pprint import pprint
+
 if sys.version_info < (3, 0):
     print("Sorry, requires Python 3.x, you are running version " + sys.version)
     exit(1)
@@ -13,7 +15,45 @@ except ImportError:
 
 from dotoollib import Config
 
-print(Config['auth']['token'])
 manager = digitalocean.Manager(token=Config['auth']['token'])
 my_droplets = manager.get_all_droplets()
-print(my_droplets)
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Digital Ocean tool')
+subparser = parser.add_subparsers()
+
+parser_list = subparser.add_parser('list')
+parser_list.add_argument('list_type',
+                         default='all',
+                         const='all',
+                         nargs='?',
+                         choices=['all', 'servers', 'storage'],
+                         help='list servers, storage, or both(default: %(default)s).')
+
+parser_create = subparser.add_parser('create')
+parser_create.add_argument('create_type',
+                           default='server',
+                           const='server',
+                           nargs='?',
+                           choices=['server', 'storage'],
+                           help='create server or storage(default: %(default)s).')
+
+parser_create = subparser.add_parser('action')
+parser_create.add_argument('action_type',
+                           default='server',
+                           const='server',
+                           nargs='?',
+                           choices=['shutdown', 'start', 'reboot'],
+                           help='Server actions (default: %(default)s).')
+
+parser_create = subparser.add_parser('status')
+parser_create.add_argument('request_type',
+                           default='status',
+                           const='status',
+                           nargs='?',
+                           choices=['status'],
+                           help='Requst server status.')
+
+args = parser.parse_args()
+pprint(vars(args))
